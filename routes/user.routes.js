9f7f6bacc64f8ddf0cd32acdad01aa63c6efb8f4
Antonio Iñiguez <antonioiniher@icloud.com>
2023-11-13 15:24:06 +0100

    Css Changes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 const User = require('./../models/User.model')
-const { isLoggedIn, checkOwnerOr } = require('../middleware/route-guard')
+const { isLoggedIn, checkRole, checkOwnerOr } = require('../middleware/route-guard')
+const { formatDate } = require('../utils/date')
 
 router.get('/lista', isLoggedIn, (req, res, next) => {
 
@@ -19,36 +20,39 @@ router.get('/:user_id', isLoggedIn, (req, res, next) => {
 
     User
         .findById(user_id)
-        .then(user => res.render('user/profile', { user, isLogged: req.session.currentUser, isLoggedOut: !req.session.currentUser }))
+        .then(user => res.render('user/profile', {
+            user,
+            isLogged: req.session.currentUser,
+            isLoggedOut: !req.session.currentUser,
+            isAdmin: req.session.currentUser.role === 'ADMIN',
+            isOwner: req.session.currentUser._id === user_id && req.session.currentUser.role != 'ADMIN'
+        }))
         .catch(err => next(err))
 
 })
 
-
-
-
-
-router.get("/:user_id/editar", (req, res, next) => {
+router.get("/:user_id/editar", checkOwnerOr('ADMIN'), (req, res, next) => {
     const { user_id } = req.params
     User
         .findById(user_id)
-        .then(user => res.render("user/edit-profile", user))
+        .then(user => res.render("user/edit-profile", {
+            user,
+            isLogged: req.session.currentUser,
+            isLoggedOut: !req.session.currentUser
+        }))
         .catch(err => console.log(err))
 })
 
-router.post("/:user_id/editar", (req, res, next) => {
+router.post("/:user_id/editar", checkOwnerOr('ADMIN'), (req, res, next) => {
     const { user_id } = req.params
     const { username, email, imageUrl, birthday, description } = req.body
     User
         .findByIdAndUpdate(user_id, { username, email, imageUrl, birthday, description })
-        .then(res.redirect("/usuario/lista"))
+        .then(() => res.redirect("/usuario/lista"))
         .catch(err => console.log(err))
 })
 
-
-
-
-router.post('/:user_id/eliminar', (req, res, next) => {
+router.post('/:user_id/eliminar', checkRole('ADMIN'), (req, res, next) => {
 
     const { user_id } = req.params
 
@@ -56,6 +60,7 @@ router.post('/:user_id/eliminar', (req, res, next) => {
         .findByIdAndDelete(user_id)
         .then(() => res.redirect('user/list'))
         .catch(err => next(err))
+
 })
 
 module.exports = router
