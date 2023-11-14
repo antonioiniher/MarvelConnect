522d0f90f52177136e9d28d77808296141dd9b29
Lucia Sanz <lu.sanz.galvez@gmail.com>
2023-11-14 15:22:17 +0100

    dia4

---
+++ public/js/place-map.js
@@ -0,0 +1,37 @@
+const ironhackCoords = { lat: 40.392521370648154, lng: - 3.6989879718518366 }
+let myMap
+
+const hiddenId = document.getElementById('identific').value
+
+
+function init() {
+    renderMap()
+    getPlacesFromAPI()
+}
+
+function renderMap() {
+    myMap = new google.maps.Map(document.querySelector("#myMap"), {
+        zoom: 13,
+        center: ironhackCoords
+    })
+}
+
+function getPlacesFromAPI() {
+    axios
+        .get(`/api/events/${hiddenId}`)
+        .then(places => printPlacesMarkers(places.data))
+        .catch(err => console.log(err))
+}
+
+function printPlacesMarkers(place) {
+
+
+    const position = { lat: place.place.coordinates[1], lng: place.place.coordinates[0] }
+
+    new google.maps.Marker({
+        map: myMap,
+        position,
+        title: place.name
+    })
+
+}
