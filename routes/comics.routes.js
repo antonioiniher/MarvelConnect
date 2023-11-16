262864c8baf10cebfe82c52579e33e573c996893
Lucia Sanz <lu.sanz.galvez@gmail.com>
2023-11-16 15:30:19 +0100

    cleanroutes

--- routes/comics.routes.js
+++ routes/comics.routes.js
@@ -4,17 +4,8 @@ const router = express.Router()
 const marvelService = require('../services/characters.services')
 const { isLoggedIn } = require('../middleware/route-guard')
 
-// TODO OPCIONAL: DESACOPLAR BUSQUEDA
+
 router.get("/", isLoggedIn, (req, res, next) => {
-
-    const { name } = req.query
-
-    if (name) {
-        marvelService
-            .getComicsByName(name)
-            .then(response => res.render('comics/list', { comics: response.data.data.results }))
-            .catch(err => next(err))
-    }
 
     marvelService
         .getAllComics()
@@ -25,6 +16,18 @@ router.get("/", isLoggedIn, (req, res, next) => {
         })
         .catch(err => next(err))
 
+})
+
+router.get('/filtroNombre', (req, res, next) => {
+
+    const { name } = req.query
+
+    if (name) {
+        marvelService
+            .getComicsByName(name)
+            .then(response => res.render('comics/list', { comics: response.data.data.results }))
+            .catch(err => next(err))
+    }
 })
 
 router.get("/detalles", isLoggedIn, (req, res, next) => {
